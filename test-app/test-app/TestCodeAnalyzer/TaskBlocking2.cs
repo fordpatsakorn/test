6c6a9473fda360139720822bfa12aa166ef6591d
fordpatsakorn <patsakorn_b@flowaccount.com>
2025-07-21 15:02:37 +0700

    Update TaskBlocking2.cs

--- test-app/test-app/TestCodeAnalyzer/TaskBlocking2.cs
+++ test-app/test-app/TestCodeAnalyzer/TaskBlocking2.cs
@@ -6,10 +6,10 @@ public class TaskBlocking2
 {
     void Example()
     {
-        Console.WriteLine("Test");
+        Console.WriteLine("The error can be detected at line level");
         Task task1 = Task.CompletedTask;
         Task<int> task2 = Task.FromResult(42);
-        task1.Wait(); // Should have warning
+        task1.Wait(); // I touched this line, so it should have warning
         var result = task2.Result; // No warning, because i didn't touch
       
     }
